6757182bccf149933a868ddba438cbbb7923ca15
NickvanderRee <112868944+NickvanderRee@users.noreply.github.com>
2022-11-23 12:19:13 +0100

    Add files via upload

---
+++ PHP_opdrachten/Opdracht1_PHP/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+</head>
+<body>
+<?php
+echo "<h1>Hallo 1K!</h1>";
+echo "<p>Dit is de eerste opdracht met PHP.</p>";
+
+$txt = "Hello world";
+
+echo $txt;
+
+?>
+</body>
+</html>
---
+++ PHP_opdrachten/opdracht3_PHP/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+    <body>
+        <?php
+            $tekst1 = "<h1>Vrijdag</h1>";
+            $tekst2 = "<p>Bijna weekend</p>";
+
+            echo $tekst1;
+            echo $tekst2;
+
+            $getal1 = 5;
+            $getal2 = 10;
+    
+            print "$getal1</br>";
+
+            print $getal2;
+
+        ?>
+    </body>
+</html>
